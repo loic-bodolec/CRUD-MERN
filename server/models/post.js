18b0d45e879d7e2f2ae23d406945f60242444835
loicbdev <loicbodolec@msn.com>
2021-10-06 09:19:22 +0200

    minor update in models and adding commnts

--- server/models/post.js
+++ server/models/post.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const postSchema = new mongoose.Schema({
-    author: String,
+// Defining your schema
+// Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+const postSchema = new Schema({
+    author: String, // String is shorthand for {type: String}
     title: String,
     article: String,
 }, { timestamps: true });
 
+// Creating a model
+// To use our schema definition, we need to convert our blogSchema into a Model we can work with. To do so, we pass it into mongoose.model(modelName, schema)
 const Post = mongoose.model('post', postSchema);
 
 module.exports = Post;

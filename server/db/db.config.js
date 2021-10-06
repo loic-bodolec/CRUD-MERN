0d4fdba77549ff664d3bbc291f4c66b554f95641
loicbdev <loicbodolec@msn.com>
2021-10-06 16:10:35 +0200

    update config for mongodb in localhost

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+// MongoDB Atlas :
+// mongoose.connect(
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster2.hhvgw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+//   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+// );
+
+
+// localhost:
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster2.hhvgw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+.then(() => console.log('Connected'))
+.catch(err => console.log(err));
